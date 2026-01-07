d7f73e2cc10dd067ba9550457a3df3e2bd2c78cd
1234321blue <djeremiahyang@gmail.com>
2026-01-06 20:31:09 -0800

    good mini work me :)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,7 +18,22 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        
+        if(Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.UpArrow))
+        {
+            _playerTransform.Translate(Vector2.up*_speed*Time.deltaTime);
+        }
+         if(Input.GetKey(KeyCode.S)||Input.GetKey(KeyCode.DownArrow))
+        {
+            _playerTransform.Translate(Vector2.down*_speed*Time.deltaTime);
+        }
+         if(Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.LeftArrow))
+        {
+            _playerTransform.Translate(Vector2.left*_speed*Time.deltaTime);
+        }
+         if(Input.GetKey(KeyCode.D)||Input.GetKey(KeyCode.RightArrow))
+        {
+            _playerTransform.Translate(Vector2.right*_speed*Time.deltaTime);
+        }
     }
 
     public void PlantSeed ()
